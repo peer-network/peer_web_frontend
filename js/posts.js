911b70dfe9f066708ff92c91a82e5d5f8ddb44c2
Yungsegz <segunadelusi303@gmail.com>
2025-05-26 10:05:25 +0200

    feat(SearchBox-refactoring): add new changes to the functionality of the search box inputs

--- js/posts.js
+++ js/posts.js
@@ -21,6 +21,9 @@ async function getPosts(offset, limit, filterBy, title = "", tag = null, sortby 
   } else {
     title = "";
   }
+  if (typeof tag === "string") {
+    tag = sanitizeString(tag);
+  }
   // tag = sanitizeString(tag);
   // tag = typeof tag === "string" ? sanitizeString(tag) : "";
   if (!sortby) sortby = "NEWEST";
@@ -30,9 +33,7 @@ async function getPosts(offset, limit, filterBy, title = "", tag = null, sortby 
       limit: ${limit},
       offset: ${offset},
       filterBy: [${filterBy}],`;
-      
-  postsList += (tag && tag.length >= 2) ? `, tag: "${tag}"` : "";
-
+  postsList += (tag && tag.length >= 3) ? `, tag: "${tag}"` : "";
   postsList += (title && title.length >= 2) ? `, title: "${title}"` : "";
 
   postsList += (userID !== null) ? `, userid: "${userID}"` : "";
