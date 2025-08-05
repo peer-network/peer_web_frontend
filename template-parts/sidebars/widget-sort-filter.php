f95cc1adab4ddd4926e8daf9ff9d566cb50cf8cb
Yungsegz <segunadelusi303@gmail.com>
2025-08-05 16:52:54 +0200

    feat(sort-filters-update): add new feature to improve the flow of the sort filters and also the design

--- template-parts/sidebars/widget-sort-filter.php
+++ template-parts/sidebars/widget-sort-filter.php
@@ -1,29 +1,47 @@
 <div class="widget">
-    	<h3 class="widget-title">Sorting</h3>
-    	<div class="widget-inner widget-type-box">
-        <form  class="filterContainer">
-        	<div class="filterGroup">
-                <input id="filterMostLiked" sortby="LIKES" class="chkMost" type="radio" name="sortby" />
-                <label for="filterMostLiked" class="filterButton most" title="Sort by most liked">
-                    <img src="svg/post-like.svg" alt="MostLiked filter"/>
-                    <span>Likes</span>
-                </label>
-                <input id="filterMostCommented" sortby="COMMENTS" class="chkMost" type="radio" name="sortby"/>
-                <label for="filterMostCommented" class="filterButton most" title="Sort by most commented">
-                    <img src="svg/post-comment.svg" alt="MostCommented filter"/>
-                    <span>Comments</span>
-                </label>
-                <input id="filterMostPopular" sortby="VIEWS" class="chkMost" type="radio" name="sortby" />
-                <label for="filterMostPopular" class="filterButton most" title="Sort by most popular"/>
-                    <img src="svg/popular.svg" alt="MostPopular filter">
-                    <span>Popular</span>
-                </label>
-                <input id="filterMostControversial" sortby="DISLIKES" class="chkMost" type="radio" name="sortby" />
-                <label for="filterMostControversial" class="filterButton most" title="Sort by most controversial">
-                    <img src="svg/controversial.svg" alt="MostControversial filter"/>
-                    <span>Discussing</span>
-                </label>
-            </div>
-        </form>
-    	</div>
+    	<div class="widget-head">
+            <img src="svg/sort-icon.svg" alt="">
+            <h3 class="widget-title">Sort by</h3>
+        </div>
+            <form  class="filterContainer">
+                <!-- Sort Filter -->
+                <section class="filter-section">
+                    <button type="button" class="filter-toggle filter-section-header" aria-expanded="false" aria-controls="sort-options">
+                        <div class="filter-section-container">
+                            <img src="svg/most-icon.svg" alt="" class="most-icon section-icon">
+                            <span class="section-title">Most...</span>
+                        </div>
+                        <img src="svg/content-arrow.svg" alt="" class="section-arrow">
+                    </button>
+                    <div class="filter-options content-options" id="sort-options">
+                        <div class="filterGroup">
+                            <input id="filterMostLiked" sortby="LIKES" class="chkMost" type="radio" name="sortby" />
+                            <label for="filterMostLiked" class="filterButton most" title="Sort by most liked">
+                                <img src="svg/post-like.svg" alt="MostLiked filter"/>
+                                <span>Likes</span>
+                            </label>
+                            <input id="filterMostControversial" sortby="DISLIKES" class="chkMost" type="radio" name="sortby" />
+                            <label for="filterMostControversial" class="filterButton most" title="Sort by most controversial">
+                                <img src="svg/most-dislikes.svg" alt="MostControversial filter"/>
+                                <span>Dislikes</span>
+                            </label>
+                            <input id="filterMostViewed" sortby="VIEWS" class="chkMost" type="radio" name="sortby" />
+                            <label for="filterMostViewed" class="filterButton most" title="Sort by most viewed">
+                                <img src="svg/most-views.svg" alt="MostViewed filter"/>
+                                <span>Viewed</span>
+                            </label>
+                            <input id="filterMostPopular" sortby="VIEWS" class="chkMost" type="radio" name="sortby" />
+                            <label for="filterMostPopular" class="filterButton most" title="Sort by most popular"/>
+                                <img src="svg/controversial.svg" alt="MostPopular filter">
+                                <span>Trends</span>
+                            </label>
+                            <input id="filterMostCommented" sortby="COMMENTS" class="chkMost" type="radio" name="sortby"/>
+                            <label for="filterMostCommented" class="filterButton most" title="Sort by most commented">
+                                <img src="svg/post-comment.svg" alt="MostCommented filter"/>
+                                <span>Thread</span>
+                            </label>
+                        </div>
+                    </div>
+                </section>
+            </form>
     </div>
