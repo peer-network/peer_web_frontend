abea4937e6976599ee042e64131ea2eeed761131
Khalid Meraj <khalidskrill@gmail.com>
2025-08-11 20:04:22 +0500

    fixed collapsed widgets css for victor

--- template-parts/sidebars/widget-collapse-button.php
+++ template-parts/sidebars/widget-collapse-button.php
@@ -1,4 +1,4 @@
-<div class="widget">
+<div class="widget widget-collapse-button">
     <button class="btn-transparent collapse-filters">
         <span class="collapse_btn section-title">Collapse view</span>
         <img src="svg/filter-collapse-icon.svg" alt="" class="section-icon">
