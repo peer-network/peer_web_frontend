5b24abe36aa7d51ea5b7e16ecb231d3144f5051a
LuqmanUddin0007 <luqman.afridi.849@gmail.com>
2025-08-13 10:13:10 +0200

    fixed webm issue on video-conversion

--- js/add_post.js
+++ js/add_post.js
@@ -2223,7 +2223,7 @@ async function trimVideo(background = false) {
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
     const ctx = canvas.getContext("2d");
-    let mimeType = MediaRecorder.isTypeSupported("video/webm;codecs=vp9") ? "video/webm;codecs=vp9" : MediaRecorder.isTypeSupported("video/webm;codecs=vp8") ? "video/webm;codecs=vp8" : MediaRecorder.isTypeSupported("video/webm") ? "video/webm" : MediaRecorder.isTypeSupported("video/mp4") ? "video/mp4" : ""; // leer = Default, aber meist WebM
+    let mimeType = "video/mp4"; 
 
     // Canvas streamen
     const stream = canvas.captureStream();
@@ -2255,7 +2255,7 @@ async function trimVideo(background = false) {
 
     // Nach Aufnahme: Download anbieten
     rec.onstop = async () => {
-      const blob = new Blob(chunks, { type: "video/webm" });
+      const blob = new Blob(chunks, { type: "video/mp4" });
       // const url = URL.createObjectURL(blob);
       const base64 = await blobToBase64(blob);
       cuttedVideo = base64; // Geschnittenes Video in Base64 speichern
