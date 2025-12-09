a7f088ede35e92282e4852ba4b10170e84d7f5df
khalidmeraj <khalidmeraj786@gmail.com>
2025-12-09 12:38:40 +0100

    Fixing the profile visiblity according to the status

--- js/viewprofile.js
+++ js/viewprofile.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const params = new URLSearchParams(window.location.search);
   const userID = params.get('user') || params.get("testid");
-  //const visibilityStatus = params.get("uservisibility");
+  const testvisibilityStatus = params.get("uservisibility");
   const urlIsReported = params.get("isreported");
 
   getProfile(userID).then(userprofile => {
     
-    
+    const viewProfile = document.querySelector('.view-profile');
     if(userprofile.ResponseCode=='30201' && userprofile.status=='error'){
       //No User Found;
       window.location.href = "404.php";
@@ -14,9 +14,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     /*-- handling profile visibility----*/
-    const visibilityStatus = userprofile.affectedRows.visibilityStatus || 'NORMAL';
-    //const visibilityStatus = "ILLEGAL";
+    let visibilityStatus = userprofile.affectedRows.visibilityStatus || 'NORMAL';
+
+   
     const hasActiveReports = userprofile.affectedRows.hasActiveReports || false;
+   
     let isReportedByYou = userprofile.affectedRows.isreported;
 
     if (urlIsReported !== null) {
@@ -26,31 +28,33 @@ document.addEventListener("DOMContentLoaded", () => {
     if (isReportedByYou === true) {
       disableReportButton();
     }
-     const viewProfile = document.querySelector('.view-profile');
+    
+    /*-- for testing profile report and visibility----*/  
+          if(testvisibilityStatus){
+            visibilityStatus = testvisibilityStatus;
+          }
+
+    /*-- End : testing profile report and visibility----*/
+    if(viewProfile) {
+     viewProfile.classList.add("profile_visibilty_"+visibilityStatus.toLowerCase());
+    }
     if(visibilityStatus === 'ILLEGAL' || visibilityStatus === 'illegal'){
-     
-      if(viewProfile) {
-        viewProfile.classList.remove("REPORTED_PROFILE");
-        viewProfile.classList.remove("HIDDEN_PROFILE");
-        viewProfile.classList.add("illegal_profile");
-      }
-    } else if(visibilityStatus === 'HIDDEN' || visibilityStatus === 'hidden'){
+      userprofile.affectedRows.username='removed';
+      userprofile.affectedRows.slug='removed';
+      userprofile.affectedRows.img='svg/noname.svg';
+     const reportButton = document.querySelector('.report_profile');
+     reportButton.remove();
       
-      if(viewProfile) {
-        viewProfile.classList.remove("REPORTED_PROFILE");
-        viewProfile.classList.add("visibility_hidden");
-        viewProfile.classList.add("HIDDEN_PROFILE");
-      }
-    } else if(hasActiveReports) {
+    } 
+    if(hasActiveReports) {
       
       if (viewProfile) {
-        viewProfile.classList.remove("visibility_hidden");
-        viewProfile.classList.add('REPORTED_PROFILE');
+       viewProfile.classList.add('profile_has_reported');
       }
       addReportedBadge();
     }
     /*-- End : handling profile visibility----*/
-
+    const profileInfo = document.querySelector(".profile_info");
     const profileimg = document.getElementById("profilbild2");
     const username2 = document.getElementById("username2");
     const slug2 = document.getElementById("slug2");
@@ -127,17 +131,19 @@ document.addEventListener("DOMContentLoaded", () => {
           return response.text();
         })
         .then(biographyText => {
-          document.getElementById("biography2").innerText = biographyText;
+          biography2.innerText = biographyText;
         })
         .catch(error => {
           console.error("Error loading biography:", error);
-          document.getElementById("biography2").innerText = "Biography not available";
+          biography2.innerText = "Biography not available";
         });
     } 
 
     /*---illegal Frame content */
       if(visibilityStatus === 'ILLEGAL' || visibilityStatus === 'illegal'){
-        const profileInfo = document.querySelector(".profile_info");
+        biography2.innerText='removed';
+        profileimg.remove();
+        
           
         const illegalProfileHTML = `
         <div class="illegal_profile_frame xl_font_size">
@@ -154,9 +160,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     /*---Hidden Account Frame */
     if(visibilityStatus === 'HIDDEN' || visibilityStatus === 'hidden'){
-      
-      const profileInfo = document.querySelector(".profile_info");
-      
+       
       const hiddenAccountHTML = `
         <div class="hidden_account_frame">
           <div class="hidden_content">
@@ -185,9 +189,9 @@ document.addEventListener("DOMContentLoaded", () => {
           e.preventDefault();
           
           hiddenFrame.remove();
-          const viewProfile = document.querySelector('.view-profile');
+          
           if(viewProfile) {
-            viewProfile.classList.remove('visibility_hidden');
+            viewProfile.classList.remove('profile_visibilty_hidden');
           }
         });
       }
