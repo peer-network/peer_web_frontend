448fd4e46b4257f659bba49d6cf903460693d384
Luqman <luqman.afridi.849@gmail.com>
2026-01-27 12:41:28 +0100

    chore: complete firebase config migration by moving projectId to env.js

--- js/firebase_config.js
+++ js/firebase_config.js
@@ -1,7 +1,7 @@
 const firebaseConfig = {
   apiKey: typeof FIREBASE_API_KEY !== 'undefined' ? FIREBASE_API_KEY : "",
   authDomain: typeof authDomain !== 'undefined' ? authDomain : "",
-  projectId: "peer-de113",
+  projectId: typeof projectId !== 'undefined' ? projectId : "",
   storageBucket: typeof storageBucket !== 'undefined' ? storageBucket : "",
   messagingSenderId: typeof messagingSenderId !== 'undefined' ? messagingSenderId : "",
   appId: typeof FIREBASE_APP_ID !== 'undefined' ? FIREBASE_APP_ID : "",
